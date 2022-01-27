2de26a0501fbbc0b0005505999418a87332dd9de
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-27 12:15:12 +0000

    fixed bug 2

--- test_driven_development/debugging_fizzbuzz/exercise_2/lib/cipher.rb
+++ test_driven_development/debugging_fizzbuzz/exercise_2/lib/cipher.rb
@@ -1,17 +1,19 @@
 class Cipher
 
   def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     ciphertext_chars = plaintext.chars.map do |char|
+      p char
+      p cipher.find_index(char)
       (65 + cipher.find_index(char)).chr
     end
     ciphertext_chars.join
   end
 
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     plaintext_chars = ciphertext.chars.map do |char|
-      cipher[65 - char.ord]
+      cipher[char.ord - 65]
     end
     plaintext_chars.join
   end
